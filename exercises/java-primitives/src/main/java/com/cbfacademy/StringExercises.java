d1804573c6865c9984de3800ece63b24cd08aa78
dolapo adebanjo <dolapomola@gmail.com>
2023-05-23 14:46:14 +0100

    exercise on strings

--- exercises/java-primitives/src/main/java/com/cbfacademy/StringExercises.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/StringExercises.java
@@ -4,20 +4,41 @@ public class StringExercises {
 
     public String fromCharacters(char[] characters) {
         // TODO - Write code that creates a String from the input array of characters
-        throw new RuntimeException("Not implemented");
+       String stringValue = characters.toString();
+       return stringValue;
+        // throw new RuntimeException("Not implemented");
     }
 
     public Long howMany(String text, Character character) {
         // TODO - Write code to determine how many of the input ${character} are contained in the input ${text}
-        throw new RuntimeException("Not implemented");
+        char[] characterOfStrings = text.toCharArray();
+        long countIdenticalCharacter = 0;
+        for(int i = 0; i < text.length(); i++){
+             if(characterOfStrings[i] == character){
+              countIdenticalCharacter++;
+             }
+        }
+        System.out.println(countIdenticalCharacter + " is the number of " + character + " present.");
+        return countIdenticalCharacter;
+        // throw new RuntimeException("Not implemented");
     }
 
     public Boolean isPalindrome(String word) {
         // TODO - Write code to determine whether the input ${word} is a palindrome
-        throw new RuntimeException("Not implemented");
+        StringBuilder sb = new StringBuilder(word);
+        String reverseWord = sb.reverse().toString();
+      if(word.equals(reverseWord)){
+        System.out.println("true");
+        return true;
+      }
+      System.out.println("false");
+      return false;
     }
 
     public String getName() {
         return "String Exercises";
+    }
+
+    public void fromCharacters(char c, char d, char e, char f) {
     }
 }
