86c9884a7d01a7d437d4b4baa13113c43f6c6cd2
dolapo adebanjo <dolapomola@gmail.com>
2023-06-05 17:26:34 +0100

    module 3 tasks

--- exercises/flow-control/src/main/java/com/cbfacademy/App.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/App.java
@@ -1,7 +1,50 @@
 package com.cbfacademy;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("\n");
+        List<Integer> linkedList = new LinkedList<>();
+        linkedList.add(5);
+        linkedList.add(6);
+        linkedList.add(8);
+        linkedList.add(2);
+        linkedList.add(9);
+        linkedList.add(15);
+        linkedList.add(45);
+        linkedList.add(9);
+        linkedList.add(60);
+        linkedList.add(25);
+        linkedList.add(17);
+        linkedList.add(30);
+        linkedList.add(29);
+        linkedList.add(7);
+
+        ArrayList<Integer> intArray = new ArrayList<>();
+        intArray.add(2);
+        intArray.add(3);
+        intArray.add(4);
+        intArray.add(5);
+        intArray.add(6);
+        intArray.add(7);
+        intArray.add(8);
+        intArray.add(9);
+
+        FlowControlExercises flowControlExercises = new FlowControlExercises();
+        flowControlExercises.fizzBuzz(linkedList);
+        System.out.println("\n");
+        flowControlExercises.whichMonth(6);
+        flowControlExercises.whichMonth(20);
+        flowControlExercises.whichMonth(8);
+        flowControlExercises.whichMonth(1);
+        flowControlExercises.whichMonth(37);
+        System.out.println("\n");
+        flowControlExercises.sumOfOddsAndSumOfEvens();
+        System.out.println("\n");
+        flowControlExercises.reverse(intArray);
     }
 }
--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -1,44 +1,121 @@
 package com.cbfacademy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class FlowControlExercises {
 
     public List<String> fizzBuzz(List<Integer> numbers) {
-        // TODO - Implement this method such that
-        //  it creates a list where for each element of the input list ${numbers}
-        //  - if the ${element} is divisible by 3, it adds “Fizz” to the list
-        //  - if the ${element} is divisible by 5, it adds “Buzz” to the list
-        //  - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
-        //  - it adds the element to the list in any other case
-        //  - it returns the constructed list
-        throw new RuntimeException("Not implemented");
+        List<String> lStringsList = new LinkedList<>();
+       for(int i = 0; i < numbers.size(); i++){
+       if(numbers.get(i) % 5 == 0 && numbers.get(i) % 3 == 0){
+            lStringsList.add("FizzBuzz");
+         }else if(numbers.get(i) % 3 == 0){
+            lStringsList.add("Fizz");
+        }else if(numbers.get(i) % 5 == 0){
+            lStringsList.add("Buzz");
+         }else{
+            lStringsList.add(String.valueOf(numbers.get(i)));
+         }
+       }
+        System.out.println(lStringsList + "::: lstringslist");
+         return lStringsList;
     }
 
     public String whichMonth(Integer number) {
         // TODO - Implement this method such that
-        //  - it returns the month corresponding to the input ${number}
-        //  - if the ${number} is invalid, the method should return "Invalid month number"
-        throw new RuntimeException("Not implemented");
+        String value;
+        switch (number) {
+            case 1:
+             value = "January";
+            break;
+            case 2:
+             value = "February";
+            break;
+            case 3:
+             value = "March";
+            break;
+            case 4:
+             value = "April";
+            break;
+            case 5:
+             value = "May";
+            break;
+            case 6:
+             value = "June";
+            break;
+            case 7:
+             value = "July";
+            break;
+            case 8:
+             value = "August";
+            break;
+            case 9:
+             value = "September";
+            break;
+            case 10:
+             value = "October";
+            break;
+            case 11:
+             value = "November";
+            break;
+            case 12:
+             value = "December";
+            break;
+            default:
+            value = "Invalid month number";
+        }
+        System.out.println(value + " ::: value of month");
+        return value;
     }
 
     public Map<String, Integer> sumOfOddsAndSumOfEvens() {
         // TODO - Implement this method such that
         //  - creates and initialises a list of 100 numbers - from 1 to 100
+        List<Integer> lIntegers = new LinkedList<>();
+        int calculatedSumOfEvens = 0;
+        int calculatedSumOfOdds = 0;
+        int numbers = 1;
+        while(numbers <= 100){
+            lIntegers.add(numbers);
+            ++numbers;
+        }
         //  - determines the sum of all the even numbers in the list
+        for(int i = 0; i < lIntegers.size(); i++){
+            if(lIntegers.get(i) % 2 == 0){
+                calculatedSumOfEvens += lIntegers.get(i);
+            }
+        }
+        System.out.println(calculatedSumOfEvens + "final even sum value");
+
         //  - determines the sum of all the odd numbers in the list
-        //  - returns a map with two entries:
-        //      {"SumOfEvens", calculatedSumOfEvens}, {"SumOfOdds", calculatedSumOfOdds}
-        throw new RuntimeException("Not implemented");
+        for(int i = 0; i < lIntegers.size(); i++){
+            if(lIntegers.get(i) % 2 == 1){
+                calculatedSumOfOdds += lIntegers.get(i);
+            }
+        }
+        System.out.println(calculatedSumOfOdds + "final odd sum value");
+      
+        Map<String, Integer> map = new HashMap<>();
+        map.put("SumOfEvens", calculatedSumOfEvens);
+        map.put("SumOfOdds", calculatedSumOfOdds);
+        return map;
     }
 
     public List<Integer> reverse(ArrayList<Integer> numbers) {
-        // TODO - Implement this method such that
-        //  - it takes an array list of integers
+        System.out.println(numbers + " ::: numbers");
+
+        List<Integer> reverseNumbers = new LinkedList<>();
         //  - it returns the list in reverse order
-        throw new RuntimeException("Not implemented");
+        for(int i = numbers.size() - 1; i >= 0; i--){
+           reverseNumbers.add(numbers.get(i));
+        }
+        System.out.println(reverseNumbers + " ::: reverseNumbers");
+
+        return reverseNumbers;
     }
 
     public String getName() {
